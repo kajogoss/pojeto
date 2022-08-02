982da7907106d4c79884deedceda66c3494d5bb3
kajogoss <101646566+kajogoss@users.noreply.github.com>
2022-08-02 11:34:31 -0300

    pojeto
    hgdb

---
+++ perrito/perrito/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perrito
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pessoa p = new Pessoa();
+
+			Console.WriteLine(p.ToString());
+
+			p.Nome = "Thigas";
+
+			Console.WriteLine(p);
+			Console.ReadKey();
+
+		}
+
+
+	}
+}
